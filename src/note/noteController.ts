4fdcbc5021156b35925de80669f81cd0e8c19ec9
Sijan-Giri <girisijan346@gmail.com>
2024-10-14 21:11:22 +0545

    Uploading file feature

--- src/note/noteController.ts
+++ src/note/noteController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import Note from "./noteModel";
+import envConfig from "../config/config";
 
 
 const createNote = async(req:Request,res:Response) => {
     try {
-    const file = req.file
+    const file = req.file ? `${envConfig.backendUrl}/${req.file.filename}` : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7EtqOpuewOP5rSURN8E4W8rUPhuGoPK2LKw&s"
     const {title , subtitle , description } = req.body;
     if(!title || !description) {
          res.status(400).json({
--- src/note/noteRoute.ts
+++ src/note/noteRoute.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { createNote } from "./noteController";
+import {multer , storage} from "./../middleware/multerMiddleware"
 
 const router = express.Router();
 
-router.route("/createNote").post(createNote)
+const upload = multer({storage : storage})
+router.route("/createNote").post(upload.single("file"),createNote)
 
 export default router
 
